9f85a4e3345c5ed2b9b7e63b4742d97daaefb8a1
Minh Phuong <83202708+mihphgbi@users.noreply.github.com>
2026-07-09 22:38:49 +0700

    fan

---
+++ src/Fan.java
@@ -0,0 +1,45 @@
+public class Fan {
+    public static final int SLOW = 1;
+    public static final int MEDIUM = 2;
+    public static final int FAST = 3;
+
+    private int speed = SLOW;
+    private boolean on = false;
+    private double radius = 5.0;
+    private String color = "blue";
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+    public void setOn(boolean on) {
+        this.on = on;
+    }
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+    public void setColor(String color) {
+        this.color = color;
+    }
+    public int getSpeed() {
+        return speed;
+    }
+    public boolean isOn() {
+        return on;
+    }
+    public double getRadius() {
+        return radius;
+    }
+    public String getColor() {
+        return color;
+    }
+
+    public Fan() {
+    }
+    public String toString() {
+        if (on) {
+            return "speed: " + speed + " color: " + color + " radius: " + radius + " fan is on";
+        } else {
+            return "color: " + color + " radius: " + radius + " fan is off";
+        }
+    }
+}
